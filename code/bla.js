821a49ff6fefd274f0236e1d4545006bd1d5c1f6
Lihi <you@example.com>
2020-04-13 15:29:01 +0300

    update to the new p-machine

--- code/bla.js
+++ code/bla.js
@@ -1,13 +1,22 @@
 var fs = require("fs");  // file system 
 
 
+
 var MAXVARS = 512
+
+function divide(a, b) {
+    if (a % 1 === 0 && b % 1 === 0) {
+        var c = Math.floor(a / b);
+        return c;
+    }
+    return a / b;
+}
 
 var BINARY = {
     'add': function (a, b) { return a + b },
     'sub': function (a, b) { return a - b },
     'mul': function (a, b) { return a * b },
-    'div': function (a, b) { return a / b },
+    'div': function (a, b) { return divide(a, b) },
     'equ': function (a, b) { return a == b },
     'neq': function (a, b) { return a != b },
     'les': function (a, b) { return a < b },
@@ -45,11 +54,27 @@ function str2val(s) {
     return s.indexOf('.') >= 0 ? parseFloat(s) : parseInt(s);
 }
 
-function run(lines) {
-    var stack = new Array(MAXVARS);
-    var pc = 0;
+function val2str(v) {
+    if (typeof (v) == 'number')
+        return Math.round(v * 10000) / 10000
+    return v;
+}
 
-    var result = "";
+function ret(stack, mp) {
+    var frame = stack.slice(mp);
+    stack.splice(mp, frame.length);
+    return frame
+}
+
+function run(lines) {
+    var stack = new Array();
+    var pc = 0;
+    var mp = 0;
+    var result="";
+
+    base = function (depth, mp) {
+        return depth > 0 ? base(depth - 1, stack[mp + 1]) : mp;
+    }
 
     while (pc < lines.length) {
         if (lines[pc].trim() == '') {
@@ -59,10 +84,33 @@ function run(lines) {
 
         var words = split(lines[pc], ' ');
         var cmd = words[0];
+        var depth = NaN;
+        var relativeAddress = NaN;
+
+        if (cmd == 'cupi' || cmd == 'mstf' || cmd == 'lda'
+            || cmd == 'lod' || cmd == 'str') {
+            depth = parseInt(words[words.length - 2]);
+            reladdr = parseInt(words[words.length - 1]);
+        }
 
         if (cmd in JUMPS) {
             var addr = parseInt(words[words.length - 1]);
             pc = JUMPS[cmd](stack, pc, addr);
+            continue;
+        }
+        if (cmd == 'cup') {
+            nparams = parseInt(words[words.length - 2]);
+            addr = parseInt(words[words.length - 1]);
+            mp = stack.length - nparams - 5;
+            stack[mp + 4] = pc;
+            pc = addr;
+            continue;
+        }
+        if (cmd == 'cupi') {
+            dynamic = stack[mp + 2];
+            desc = base(depth, dynamic) + reladdr;
+            stack[mp + 4] = pc;
+            pc = stack[desc];
             continue;
         }
 
@@ -78,7 +126,7 @@ function run(lines) {
         } else if (cmd == 'ind') {
             stack.push(stack[stack.pop()]);
         } else if (cmd == 'print' || cmd == 'out') {
-            result += ((stack.pop())) + "\n";
+            result+=val2str(stack.pop())+"\n";
         } else if (cmd == 'sto') {
             var rval = stack.pop();
             var lval = stack.pop();
@@ -87,9 +135,46 @@ function run(lines) {
             var a = stack.pop();
             stack[stack.length - 1] += a * parseInt(words[words.length - 1]);
         } else if (cmd == 'dec') {
-            stack[stack.length - 1]--;
+            stack[stack.length - 1] -= parseInt(words[words.length - 1]);
         } else if (cmd == 'inc') {
-            stack[stack.length - 1]++;
+            stack[stack.length - 1] += parseInt(words[words.length - 1]);
+        } else if (cmd == 'dpl') {
+            stack.push(stack[stack.length - 1]);
+        } else if (cmd == 'mst') {
+            depth = parseInt(words[words.length - 1]);
+            stack.push(NaN, base(depth, mp), mp, NaN, NaN);
+        } else if (cmd == 'mstf') {
+            desc = base(depth, mp) + reladdr;
+            stack.push(NaN, stack[desc + 1], mp, NaN, NaN);
+        } else if (cmd == 'lda') {
+            addr = base(depth, mp) + reladdr;
+            stack.push(addr);
+        } else if (cmd == 'lod') {
+            addr = base(depth, mp) + reladdr;
+            stack.push(stack[addr]);
+        } else if (cmd == 'movs') {
+            src = stack.pop();
+            size = parseInt(words[words.length - 1]);
+            while (size--)
+                stack.push(stack[src++]);
+        } else if (cmd == 'ssp') {
+            spsize = mp + parseInt(words[words.length - 1]);
+            while (stack.length < spsize)
+                stack.push(NaN);
+        } else if (cmd == 'smp') {
+            nparams = parseInt(words[words.length - 1]);
+            mp = stack.length - nparams - 5;
+        } else if (cmd == 'retp') {
+            frame = ret(stack, mp);
+            mp = frame[2];
+            pc = frame[4];
+        } else if (cmd == 'retf') {
+            frame = ret(stack, mp);
+            stack.push(frame[0]);
+            mp = frame[2];
+            pc = frame[4];
+        } else if (cmd == 'stp') {
+            break;
         } else {
             throw ("unknown command at line " + pc + ": " + lines[pc]);
         }
@@ -131,30 +216,6 @@ function parse(code) {
         console.log('Error occurred during execution: ' + err);
     }
 }
-
-// function TryParseInt(str,defaultValue) {
-//     var retValue = defaultValue;
-//     if(str !== null) {
-//         if(str.length > 0) {
-//             if (!isNaN(str)) {
-//                 retValue = parseInt(str);
-//             }
-//         }
-//     }
-//     return retValue;
-// }
-
-// function TryParseDouble(str,defaultValue) {
-//     var retValue = defaultValue;
-//     if(str !== null) {
-//         if(str.length > 0) {
-//             if (!isNaN(str)) {
-//                 retValue = parseDouble(str);
-//             }
-//         }
-//     }
-//     return retValue;
-// }
 
 function doit() {
     fs.readFile(".\\output\\generated_pcode.txt", 'utf8', function (err, data) {
